4de78967e4cbf7fbcf60052d910f12ba5ee3ee28
zaineb <103284201+zcybrgd@users.noreply.github.com>
2025-06-19 21:49:11 +0100

    ajoute decalaration variable

--- sockets/sync-chat-app/serveur.c
+++ sockets/sync-chat-app/serveur.c
@@ -18,36 +18,36 @@ la communication donc est synchrone, un message chacun à tour de rôle
 
 int main() {
     int socketID, connexion;
-    struct sockaddr_in information_server, information_client;
-    socklen_t len = sizeof(struct sockaddr_in);
-    char phrase[255];
-
-    // Création du socket
+    struct sockaddr_in information_server, information_client; //2 structures pour les info du client et server
+    socklen_t len = sizeof(struct sockaddr_in); //taille de la structure du client
+    char phrase[255]; //le buffer as in client code
+    int connexion =  0;
+    // Création de la socket tcp
     socketID = socket(AF_INET, SOCK_STREAM, 0);
     if (socketID == -1) {
         perror("socket");
         exit(-1);
     }
 
-    // Initialisation
+    // init de la structure d'adresse du serveur
     memset(&information_server, 0, sizeof(struct sockaddr_in));
     information_server.sin_family = AF_INET;
     information_server.sin_port = htons(6666);
-    information_server.sin_addr.s_addr = INADDR_ANY;
+    information_server.sin_addr.s_addr = INADDR_ANY;//accepte des connexions de tous les ip
 
-    // Bind
+    // Bind: on associe l'adresse avec ses info au socket
     if (bind(socketID, (struct sockaddr *) &information_server, sizeof(struct sockaddr)) == -1) {
         perror("bind");
         exit(-1);
     }
 
-    // Écoute
+    //Écoute des connex entrantes
     if (listen(socketID, 5) == -1) {
         perror("listen");
         exit(-1);
     }
 
-    // Accept
+    // Accept la demande du client de la connexion
     memset(&information_client, 0, sizeof(struct sockaddr_in));
     connexion = accept(socketID, (struct sockaddr *) &information_client, &len);
     if (connexion == -1) {
@@ -55,24 +55,24 @@ int main() {
         exit(-1);
     }
 
-    printf("Connexion acceptée de %s\n", inet_ntoa(information_client.sin_addr));
+    printf("Connexion acceptée de %s\n", inet_ntoa(information_client.sin_addr));//pour juse afficher l'ip du client
 
-    // Communication
+    // Communication: same loop implémentée
     do {
-        memset(phrase, 0, 255);
-        recv(connexion, phrase, 255, 0);
+        memset(phrase, 0, 255); //reset à 0 aka vide le buffer
+        recv(connexion, phrase, 255, 0); //reçoit message au client
         printf("Client > %s\n", phrase);
 
         if (strcmp(phrase, "EXIT") != 0) {
             printf("Serveur > ");
-            fgets(phrase, sizeof(phrase), stdin);
+            fgets(phrase, sizeof(phrase), stdin); // Lire la réponse du serveur
             phrase[strcspn(phrase, "\n")] = 0; // Supprimer \n
-            send(connexion, phrase, strlen(phrase), 0);
+            send(connexion, phrase, strlen(phrase), 0); //l'envoyer au clientt
         }
 
     } while (strcmp(phrase, "EXIT") != 0);
 
-    // Fermeture
+    //Fermeture gracefully de la socket proprement
     shutdown(socketID, SHUT_RDWR);
     return 0;
 }
