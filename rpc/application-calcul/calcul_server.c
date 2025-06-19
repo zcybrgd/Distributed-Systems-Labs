cd3910bbc4ae585ff8941a57925a612ee1bc348b
zaineb <103284201+zcybrgd@users.noreply.github.com>
2025-06-19 22:12:45 +0100

    modifie serveur

--- rpc/application-calcul/calcul_server.c
+++ rpc/application-calcul/calcul_server.c
@@ -1,14 +1,18 @@
-#include "calcul.h"
+#include "calcul.h" //généré par le idl
 
+// vide juste pour tester si le service est en ligne et sert aussi à réveiller le serveur
 void *
 calcul_null_1_svc(void *argp, struct svc_req *rqstp) {
     static char* result;
     return (void*) &result;
 }
 
+//la fonction la raison du remote call actually
+//argp contient les données reçues (arg1, arg2)
 reponse *
 calcul_addition_1_svc(data *argp, struct svc_req *rqstp) {
     static reponse result;
+    //static : les données persistent après le retour (important car RPC retourne un pointeur)
     unsigned int max;
 
     result.errno = 0;
@@ -21,3 +25,5 @@ calcul_addition_1_svc(data *argp, struct svc_req *rqstp) {
 
     return &result;
 }
+
+//rpcinfo interroge le portmapper pour vérifier que le service RPC est actif
