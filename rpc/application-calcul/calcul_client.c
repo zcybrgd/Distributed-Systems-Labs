1f35fe9e12c11eee2b959e4f02fa45d55d54c12e
zaineb <103284201+zcybrgd@users.noreply.github.com>
2025-06-19 22:10:31 +0100

    client checked

--- rpc/application-calcul/calcul_client.c
+++ rpc/application-calcul/calcul_client.c
@@ -1,17 +1,19 @@
+//donc le but ici c que le client appelle une fonction comme si elle était locale, mais elle s’exécute sur le serveur à distance
+//RPC va nous masquer la complexité réseau: no besoin de créer des sockets, de gérer l’envoi manuel des messages, ni la sérialisation/désérialisation des données 
 #include <limits.h>
 #include "calcul.h"
 
-CLIENT *clnt;
+CLIENT *clnt; //structure contenant la connexion vers le serveur
 
 void test_addition(uint param1, uint param2) {
     reponse *resultat;
     data parametre;
-
+    //préparation des arguments (typical)
     parametre.arg1 = param1;
     parametre.arg2 = param2;
 
     printf("Appel de CALCUL_ADDITION avec: %u et %u\n", parametre.arg1, parametre.arg2);
-
+    //un appel réseau UDP avec sérialisation automatique
     resultat = calcul_addition_1(&parametre, clnt);
     if (resultat == NULL) {
         clnt_perror(clnt, "Échec appel distant");
@@ -31,13 +33,14 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s serveur_host\n", argv[0]);
         exit(1);
     }
-
+    //on établit une connex RPC avec CALCUL, VERSION_UN → viennent du fichier .x et on choisit UDP, mais ça peut être "tcp" aussi
+    //elle crée le stub client
     clnt = clnt_create(argv[1], CALCUL, VERSION_UN, "udp");
     if (clnt == NULL) {
         clnt_pcreateerror(argv[1]);
         exit(1);
     }
-
+     // test des 2 cas 
     test_addition(UINT_MAX - 15, 10);
     test_addition(UINT_MAX, 10);
 
